238f457c4dfd581d84fa4fe2fc3184ade35abf2a
Dev-Ninehermits <dev@ninehermits.com>
2025-06-12 11:51:12 +0530

    ada

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1749709259,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bts-sheet0.png",
+		"images/opponent-sheet0.png",
+		"images/opponent-sheet1.png",
+		"images/bullet-sheet0.png",
+		"images/redlightgreenlighthowtosheet-sheet0.png",
+		"images/redlightgreenlighthowtosheet-sheet1.png",
+		"images/playsheet-sheet0.png",
+		"images/border_up-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
